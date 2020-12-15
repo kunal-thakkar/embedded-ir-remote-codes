d0a4353b85d778c97fb42f1aa7ec983e4bc2f516
kunal-thakkar <kunalmthakkar@gmail.com>
2020-12-15 14:38:41 +0530

    Added remote codes

---
+++ china_generic.h
@@ -0,0 +1,24 @@
+//Remote codes for china generic remote widely used for arduino
+
+#define KEY_CH_PLUS   0xFFA25D
+#define KEY_CH        0xFF629D
+#define KEY_CH_MINUS  0xFFE21D
+#define KEY_PREV      0xFF22DD
+#define KEY_NEXT      0xFF02FD
+#define KEY_PLAY      0xFFC23D
+#define KEY_VOL_PLUS  0xFFE01F
+#define KEY_VOL_MINUS 0xFFA857
+#define KEY_EQ        0xFF906F
+#define KEY_ZERO      0xFF6897
+#define KEY_HUN_PLUS  0xFF9867
+#define KEY_TWO_PLUS  0xFFB04F
+#define KEY_ONE       0xFF30CF
+#define KEY_TWO       0xFF18E7
+#define KEY_THREE     0xFF7A85
+#define KEY_FOUR      0xFF10EF
+#define KEY_FIVE      0xFF38C7
+#define KEY_SIX       0xFF5AA5
+#define KEY_SEVEN     0xFF42BD
+#define KEY_EIGHT     0xFF4AB5
+#define KEY_NINE      0xFF52AD
+#define KEY_REPEAT    0xFFFFFFFF
---
+++ china_mp3_1.h
@@ -0,0 +1,20 @@
+//Remote codes for china MP3 player
+
+#define KEY_POWER		0xFF48B7
+#define KEY_PLAY		0xFF7887
+#define KEY_SCAN		0xFF906F
+#define KEY_SEARCH_P	0xFFC03F
+#define KEY_SEARCH_M	0xFF40BF
+#define KEY_PREV		0xFFEE86
+#define KEY_VOL_MIUNUS	0xFFA05F
+#define KEY_VOL_PLUS	0xFF609F
+#define KEY_ONE			0xFF50AF
+#define KEY_TWO			0xFFD827
+#define KEY_THREE		0xFFF807
+#define KEY_FOUR		0xFF30CF
+#define KEY_FIVE		0xFFB04F
+#define KEY_SIX			0xFF708F
+#define KEY_SEVEN		0xFF00FF
+#define KEY_EIGHT		0xFFF00F
+#define KEY_NINE		0xFF9867
+#define KEY_REPEAT    	0xFFFFFF
